041be6ce28e3c474fd396bc9e7a832506fd84d85
BetaRayMiTo <betaraymito@gmail.com>
2022-06-11 16:27:32 -0400

    add initial files

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("hello World
+
+var canvas = document.getElementById("canvas");
+var c = canvas.getContext("2d");
+var tx = window.innerWidth;
+var ty = window.innerHeight;
+canvas.width = tx;
+canvas.height = ty;
+
+//c.lineWidth = 5;
+//c.globalAlpha = 0.5;
+
+var mousex = 0;
+var mousey = 0;
+
+addEventListener("click", function(){
+  console.log(this);
+  //mousex = event.clientX;
+  //mousey = event.clientY;
+});
